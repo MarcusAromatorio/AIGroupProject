c91503c2912b9dd18b495866155f89c9ec2016c2
Marcus Aromatorio <aromatoriom@gmail.com>
2016-03-07 20:45:38 -0500

    Integrate Rain with beginning of Zombie class behavior
    Visual sensor on the Zombie class configured to sense both graves and
    trees placed on screen
    Behavior attached to Zombie class compels the ai to seek out a tree
    entity once a hunger threshold has been exceeded via a custom behavior
    script
    Superfluous classes and scripts were removed to favor a more behavior
    tree oriented development style.
    Additions that need done:
    group avoidance,
    scene related obstacles,
    lich king,
    portals,
    spooky conga dancers

--- AIProject/Assets/AI/Actions/EatFromTree.cs
+++ AIProject/Assets/AI/Actions/EatFromTree.cs
@@ -5,25 +5,24 @@ using RAIN.Action;
 using RAIN.Core;
 
 [RAINAction]
-public class getHungrier : RAINAction
+public class EatFromTree : RAINAction
 {
+    // Potential to change hunger reduction extesions
+    private int hungerDrain;
+
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
+        hungerDrain = 100;
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 
-        // Get the current value saved as local variable
-        int hunger = ai.WorkingMemory.GetItem<int>("hunger");
-
-        // Increase the hunger level of the character
-        hunger++;
-        
-        // Save the new value to the AI memory
-        ai.WorkingMemory.SetItem<float>("hunger", hunger);
-        
+        // Three steps: Read hunger from memory, reduce by hungerDrain, save hunger to memory
+        int hungerLevel = ai.WorkingMemory.GetItem<int>("hunger");
+        hungerLevel -= hungerDrain;
+        ai.WorkingMemory.SetItem<int>("hunger", hungerLevel);
 
         return ActionResult.SUCCESS;
     }
--- AIProject/Assets/Scripts/Zombie.cs
+++ AIProject/Assets/Scripts/Zombie.cs
@@ -1,18 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RAIN.Core;
 
 public class Zombie : MonoBehaviour {
 
     public GameObject graveObject;
     public GameObject treeObject;
+    private AIRig zombieAiRig;
+    private AI zombieAi;
+
 
 	// Use this for initialization
 	void Start () {
-	
+        zombieAiRig = gameObject.GetComponentInChildren<AIRig>();
+        zombieAi = zombieAiRig.AI;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    // Methods to set behavior targets reference values
+    public void SetGraveTarget(GameObject grave)
+    {
+        graveObject = grave;
+        zombieAi.WorkingMemory.SetItem<GameObject>("targetGrave", graveObject);
+    }
+
+    public void SetTreeTarget(GameObject tree)
+    {
+        treeObject = tree;
+        zombieAi.WorkingMemory.SetItem<GameObject>("targetTree", treeObject);
+    }
+
+    // Method to stop the AI and child components from updating
+    public void Disable()
+    {
+        if(zombieAi.Started)
+        {
+            zombieAi.IsActive = false;
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Method to start the AI and child components after being disabled
+    public void Enable()
+    {
+        if(zombieAi.Started &!zombieAi.IsActive)
+        {
+            zombieAi.IsActive = true;
+            gameObject.SetActive(true);
+        }
+    }
 }
